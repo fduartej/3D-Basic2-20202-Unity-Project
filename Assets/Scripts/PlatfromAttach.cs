0532558ecff72c835f4c7706e77721b85be8c1bf
Fred Duarte <gestion_atlassian@inteligogroup.com>
2020-11-21 13:53:19 -0500

    first commit

---
+++ Assets/Scripts/PlatfromAttach.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatfromAttach : MonoBehaviour
+{
+    public GameObject player;
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject == player) {
+
+            player.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+
+    }
+}
